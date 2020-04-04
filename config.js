8c0c9ead76ad2b4c913879b20d71f0f7f4ca6f49
LucaBlackDragon <lucablackdragon@hotmail.com>
2020-04-04 20:13:25 +0200

    Aggiornamento hash skinpack-01

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
       pak3ts: [
         {
           name: 'A4T1-skinpack-01.pk3',
-          hash: '490644880834e8c19dca5605cc4fba28',
+          hash: 'ff03b8f9df389fcd06df76b7340c02fc',
         }
       ]
     }
