a84301191dcef049cf559b21055ac3b2552c1d26
LucaBlackDragon <lucablackdragon@hotmail.com>
2020-04-04 12:20:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Luk3 Configuration 
+
+Configurazione per [luk3](https://github.com/a4t1/luk3)
---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  bundles: [
+    {
+      name: 'skinpack-01',
+      targets: ['main', 'mainta', 'maintt'],
+      pak3ts: [
+        {
+          name: 'A4T1-skinpack-01.pk3',
+          hash: '490644880834e8c19dca5605cc4fba28',
+        }
+      ]
+    }
+  ]
+}
