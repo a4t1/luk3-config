86a27eba039cc00f0ee44d6b8d7293904d423dee
LucaBlackDragon <lucablackdragon@hotmail.com>
2020-04-04 20:30:25 +0200

    Aggiunta configurazione skinpack-02

--- config.js
+++ config.js
@@ -9,6 +9,16 @@ module.exports = {
           hash: 'ff03b8f9df389fcd06df76b7340c02fc',
         }
       ]
+    },
+    {
+      name: 'skinpack-02',
+      targets: ['main', 'mainta', 'maintt'],
+      pak3ts: [
+        {
+          name: 'A4T1-skinpack-02.pk3',
+          hash: 'a63258b99e9ae5712f43826471db8468',
+        }
+      ]
     }
   ]
 }
